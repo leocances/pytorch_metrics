c1e0bfb0567792c9007d6a08666428b3da2f7534
leocances <leocances@gmail.com>
2020-07-07 14:03:35 +0200

    big refactoring, add value, mean and std computation to each metrics. They also store all the value computed during the epoch

--- metric_utils/metrics.py
+++ metric_utils/metrics.py
@@ -4,17 +4,30 @@ import numpy as np
 
 class Metrics:
     def __init__(self, epsilon=1e-10):
-        self.value_ = None
+        self.values = []
         self.accumulate_value = 0
-        self.count = 0
         self.epsilon = epsilon
 
+
     def reset(self):
-        self.accumulate_value = 0
-        self.count = 0
+        self.values = []
 
     def __call__(self, y_pred, y_true):
-        self.count += 1
+        pass
+
+    @property
+    def value(self):
+        return self.values[-1]
+
+    @property
+    def mean(self):
+        nb_value = len(self.values)
+        accumulate = sum(self.values)
+        return accumulate / nb_value
+
+    @property
+    def std(self):
+        return np.std(self.values)
 
 
 class FuncContinueAverage(Metrics):
@@ -24,18 +37,9 @@ class FuncContinueAverage(Metrics):
 
     def __call__(self, *args, **kwargs):
         super().__call__(None, None)
-        self.value_ = self.func(*args, **kwargs)
-        self.accumulate_value += self.value_
+        self.values.append(self.func(*args, **kwargs))
 
         return self
-
-    @property
-    def value(self):
-        return self.value_
-
-    @property
-    def mean(self):
-        return self.accumulate_value / self.count
 
 
 class ContinueAverage(Metrics):
@@ -44,11 +48,9 @@ class ContinueAverage(Metrics):
 
     def __call__(self, value):
         super().__call__(None, None)
-        self.value_ = value
-        self.accumulate_value += self.value_
+        self.values.append(value)
 
-        return self.accumulate_value / self.count
-
+        return self
 
 
 class BinaryAccuracy(Metrics):
@@ -61,14 +63,9 @@ class BinaryAccuracy(Metrics):
         with torch.set_grad_enabled(False):
             y_pred = (y_pred > 0.5).float()
             correct = (y_pred == y_true).float().sum()
-            self.value_ = correct / np.prod(y_true.shape)
+            self.values.append(correct / np.prod(y_true.shape))
 
-            self.accumulate_value += self.value_
-            return self.accumulate_value / self.count
-
-    @property
-    def accuracy(self):
-        return self.value_
+        return self
 
 
 class CategoricalAccuracy(Metrics):
@@ -79,10 +76,9 @@ class CategoricalAccuracy(Metrics):
         super().__call__(y_pred, y_true)
 
         with torch.set_grad_enabled(False):
-            self.value_ = torch.mean((y_true == y_pred).float())
-            self.accumulate_value += self.value_
+            self.values.append(torch.mean((y_true == y_pred).float()))
 
-            return self.accumulate_value / self.count
+        return self
 
     @property
     def accuracy(self):
@@ -98,14 +94,9 @@ class Ratio(Metrics):
 
         results = zip(y_pred, y_adv_pred)
         results_bool = [int(r[0] != r[1]) for r in results]
-        self.value_ = sum(results_bool) / len(results_bool)
-        self.accumulate_value += self.value_
+        self.values.append(sum(results_bool) / len(results_bool))
 
-        return self.accumulate_value / self.count
-
-    @property
-    def ratio(self):
-        return self.value_
+        return self
 
 
 class Precision(Metrics):
@@ -118,18 +109,19 @@ class Precision(Metrics):
 
         with torch.set_grad_enabled(False):
             dim = () if self.dim is None else self.dim
+            y_true = y_true.float()
+            y_pred = y_pred.float()
 
             true_positives = torch.sum(torch.round(torch.clamp(y_true * y_pred, 0, 1)), dim=dim)
             predicted_positives = torch.sum(torch.round(torch.clamp(y_pred, 0, 1)), dim=dim)
 
             if self.dim is None and predicted_positives == 0:
-                self.value_ = torch.as_tensor(0.0)
+                self.values.append(torch.as_tensor(0.0))
             else:
-                self.value_ = true_positives / (predicted_positives + self.epsilon)
+                self.values.append(true_positives / (predicted_positives + self.epsilon))
                 
-            self.accumulate_value += self.value_
-            return self.accumulate_value / self.value_
-
+        return self
+        
 
 class Recall(Metrics):
     def __init__(self, dim = None, epsilon=1e-10):
@@ -141,19 +133,20 @@ class Recall(Metrics):
 
         with torch.set_grad_enabled(False):
             dim = () if self.dim is None else self.dim            
+            y_true = y_true.float()
+            y_pred = y_pred.float()
             
             true_positives = torch.sum(torch.round(torch.clamp(y_true * y_pred, 0.0, 1.0)), dim=dim)
             possible_positives = torch.sum(torch.clamp(y_true, 0.0, 1.0), dim=dim)
             
             if self.dim is None and possible_positives == 0:
-                self.value_ = torch.as_tensor(0.0)
+                self.values.append(torch.as_tensor(0.0))
             else:
-                self.value_ = true_positives / (possible_positives + self.epsilon)
+                self.values.append(true_positives / (possible_positives + self.epsilon))
                 
-            self.accumulate_value += self.value_
-            return self.accumulate_value / self.count
+            return self
 
-
+        
 class FScore(Metrics):
     def __init__(self, dim = None, epsilon=np.spacing(1)):
         Metrics.__init__(self, epsilon)
@@ -172,22 +165,8 @@ class FScore(Metrics):
             self.recall = self.recall_func(y_pred, y_true)
 
             if self.dim is None and (self.precision == 0 and self.recall == 0):
-                self.value_ = torch.as_tensor(0.0)
+                self.values.append(torch.as_tensor(0.0))
             else:
-                self.value_ = 2 * ((self.precision_func.value * self.recall_func.value) / (self.precision_func.value + self.recall_func.value + self.epsilon))
+                self.values.append(2 * ((self.precision_func.value * self.recall_func.value) / (self.precision_func.value + self.recall_func.value + self.epsilon)))
                 
-            self.accumulate_value += self.value_
-            return self.accumulate_value / self.count_
-
-
-if __name__ == '__main__':
-    def test(i):
-        return i
-
-    ca_func = ContinueAverage(test)
-
-    for i in range(100):
-        ca = ca_func(i).mean
-
-    print(ca)
-    print(sum(range(100)) / 100)
+            return self
